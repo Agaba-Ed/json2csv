926487e651888a8ca4b465442fb04ceb7c8e311f
Agaba-Ed <agabsed@gmail.com>
2023-05-24 09:57:25 -0400

    Add install requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup
 
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='json2csv',
     version='0.0.3',
@@ -11,5 +7,5 @@ setup(
     author='Ed',
     author_email='agabsed@gmail.com',
     packages=['json2csv'],
-    install_requires=[],
+    install_requires=['json', 'csv'],
 )
